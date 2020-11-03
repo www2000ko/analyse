c410de0369956ae9cfd9b19a7152b0fd39fe8f9d
wbr <www2000ko@buaa.edu.cn>
2020-11-03 20:34:34 +0800

    init

---
+++ src/analyser.java
@@ -0,0 +1,9 @@
+public class analyser {
+    String rules="E -> E '+' T | T\n" +
+            "T -> T '*' F | F\n" +
+            "F -> '(' E ')' | 'i'";
+
+    public static void main(String[] args) {
+
+    }
+}
