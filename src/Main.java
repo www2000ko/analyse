b3db6ac1acc62b065b1c0f49e0080fc8a456403e
wbr <www2000ko@buaa.edu.cn>
2020-11-04 19:33:48 +0800

    gugu

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,7 @@ public class Main {
     private String ch;
     private char stkTop,chTop;
     void analyze(String ch) {
+        System.out.println(ch);
         stk="#";
         ch=ch+'#';
         stkTop=stk.charAt(0);
